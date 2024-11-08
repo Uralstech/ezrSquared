697750f27c381655b610ec04eacc4c185ce7e0eb
Udayshankar Ravikumar <uralstech@gmail.com>
2024-11-08 18:44:57 +0530

    ezr² RE v0.11.1

--- src/Shell/EzrShell.cs
+++ src/Shell/EzrShell.cs
@@ -12,7 +12,7 @@ namespace EzrSquaredCli;
 /// </summary>
 internal class Shell
 {
-    private const string Version = "0.11.0";
+    private const string Version = "0.11.1";
 
     private static bool s_showLexerOutput;
     private static bool s_showParserOutput;
